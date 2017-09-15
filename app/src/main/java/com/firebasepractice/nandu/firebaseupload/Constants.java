66f6002f4e6956359f8e8d72f3c5da80230e4c76
NandaKishorJeripothula <j.kishor.bd@gmail.com>
2017-09-15 22:51:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/firebasepractice/nandu/firebaseupload/Constants.java
@@ -0,0 +1,10 @@
+package com.firebasepractice.nandu.firebaseupload;
+
+/**
+ * Created by Nandu on 9/15/2017.
+ */
+
+public class Constants {
+        public static final String STORAGE_PATH_UPLOADS = "chat_photos/";
+        public static final String DATABASE_PATH_UPLOADS = "uploads";
+}
